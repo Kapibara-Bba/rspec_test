a128f163747335756372fe7a9fdb7613254579b8
EC2 Default User <kapibara.bba0924@gmail.com>
2020-10-04 12:31:00 +0000

    [Finish]rspec-test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
   context "データが正しく保存される" do
     before do
       @post = Post.new
@@ -9,10 +9,12 @@ RSpec.describe Post, type: :model do
       @post.content = "今日も晴れです。"
       @post.save
     end
+
     it "全て入力してあるので保存される" do
       expect(@post).to be_valid
     end
   end
+
   context "データが正しく保存されない" do
     before do
       @post = Post.new
@@ -20,6 +22,7 @@ RSpec.describe Post, type: :model do
       @post.content = "今日も晴れです。"
       @post.save
     end
+
     it "titleが入力されていないので保存されない" do
       expect(@post).to be_invalid
       expect(@post.errors[:title]).to include("can't be blank")
--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe "Posts", type: :request do
       before do
         get posts_new_path
       end
+
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
       end
-       it 'タイトルが正しく表示されていること' do
+      it 'タイトルが正しく表示されていること' do
         expect(response.body).to include("新規投稿")
       end
     end
   end
-
 end
